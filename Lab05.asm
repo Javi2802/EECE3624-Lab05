5cbaa74b6c7d127388fdd755c3e436698cc52f15
Prof. John Hutton <110941287+jfhutton@users.noreply.github.com>
2024-10-01 14:15:49 -0500

    Update with a bit more in the startup code.

--- Lab05.asm
+++ Lab05.asm
@@ -10,17 +10,26 @@
  *
  * This program...
  *
- *************************************************************************/ 
+ *************************************************************************/
 
+ /*********
+ * Interrupt Jump Table
+ *********/
 .org 0x0000                 ; next instruction address is 0x0000
                             ; (the location of the reset vector)
-rjmp main                   ; allow reset to run this program
+rjmp main					; allow reset to run this program
 
+/**********
+* Main code
+**********/
+.org 0x0020					; Move the "main" to 0x0020 to make room for ISRs
 main:                       ; jump here on reset
     ldi R16, HIGH(RAMEND)   ; initialize stack (default RAMEND = 0x10FF)
     out SPH, R16
     ldi R16, low(RAMEND)
     out SPL, R16
+
+	/* Additional Setup before Main Loop */
 
     ldi R16, (1<<DDC3)      ; set PORTC.3 pin as output via bit 3
     out DDRC, R16           ; in PORTC's data direction register
@@ -33,8 +42,8 @@ mainLoop:
     ; kill some time
     ldi R16, 40             ; R16 is outer loop counter
 outer_loop1:
-    ldi R24, low($4000)     ; load low and high parts of R25:R24 pair with
-    ldi R25, high($4000)    ; loop count by loading registers separately
+    ldi R24, low(0x4000)     ; load low and high parts of R25:R24 pair with
+    ldi R25, high(0x4000)    ; loop count by loading registers separately
     inner_loop1:
         sbiw R24, 1         ; decrement inner loop counter (R25:R24 pair)
         brne inner_loop1    ; loop back if R25:R24 isn't zero
@@ -46,8 +55,8 @@ outer_loop1:
     ; kill some more time
     ldi R16, 40             ; R16 is outer loop counter
 outer_loop2:
-    ldi R24, low($4000)     ; load low and high parts of R25:R24 pair with
-    ldi R25, high($4000)    ; loop count by loading registers separately
+    ldi R24, low(0x4000)     ; load low and high parts of R25:R24 pair with
+    ldi R25, high(0x4000)    ; loop count by loading registers separately
     inner_loop2:
         sbiw R24, 1         ; decrement inner loop counter (R25:R24 pair)
         brne inner_loop2    ; loop back if R25:R24 isn't zero
@@ -55,3 +64,9 @@ outer_loop2:
     brne outer_loop2        ; loop back if R16 isn't zero
 
     rjmp mainLoop           ; play it again, Sam...
+
+/**********
+* ISR code
+**********/
+.org 0x0200							; Load the ISR code higher than main code
+int1_isr:
